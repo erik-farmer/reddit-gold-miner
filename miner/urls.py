cd2c539018dbd7aa97b40e0bf39db0dc2c632a00
Erik Farmer <devbook@Eriks-MacBook-Pro.local>
2015-03-17 02:29:07 -0700

    fix url route for /data

--- miner/urls.py
+++ miner/urls.py
@@ -5,6 +5,6 @@ from .views import get_todays_gold_json
 from .views import MinerHomeTemplateView
 
 urlpatterns = patterns('',
-    url(r'$', MinerHomeTemplateView.as_view()),
+    url(r'^$', MinerHomeTemplateView.as_view()),
     url(r'^data', get_todays_gold_json),
 )
