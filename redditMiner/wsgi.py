388c53bbf726101bc7fb1dd793f212dcdcad1547
Erik Farmer <devbook@Eriks-MacBook-Pro.local>
2015-03-17 09:15:22 -0700

    Installation of Django White Noise for static asset serving in heroku

--- redditMiner/wsgi.py
+++ redditMiner/wsgi.py
@@ -1,15 +1,9 @@
-"""
-WSGI config for redditMiner project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
 import os
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "redditMiner.settings")
-application = Cling(get_wsgi_application())
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
